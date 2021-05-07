8e694737bc9077c5dfd4d6086ca729f5473a7c69
312585 <kate_01222@mail.ru>
2021-05-07 19:56:37 +0300

    Добавлена озвучка кнопок

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from kivy.app import App
-from kivy.core.audio import SoundLoader
 from kivy.uix.boxlayout import BoxLayout
 from audioAdapter import AudioAdapter
 
@@ -7,22 +6,28 @@ from audioAdapter import AudioAdapter
 class Tester(BoxLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.adapter = AudioAdapter('звук фон 3.mp3',True)
+        self.button_music = AudioAdapter('', False)
+        self.background_music = AudioAdapter('', True)
 
     def play_sound(self):
-        self.adapter.play_music()
+        self.button_music.play_music()
+        self.background_music.play_music()
 
     def stop_sound(self):
-        self.adapter.stop_music()
+        self.button_music.play_music()
+        self.background_music.stop_music()
 
     def increase_volume(self):
-        self.adapter.increase_volume()
+        self.button_music.play_music()
+        self.background_music.increase_volume()
 
     def reduce_volume(self):
-        self.adapter.reduce_volume()
+        self.button_music.play_music()
+        self.background_music.reduce_volume()
 
     def set_volume(self):
-        self.adapter.set_volume(0.5)
+        self.button_music.play_music()
+        self.background_music.set_volume(0.5)
 
 
 class SampleApp(App):
